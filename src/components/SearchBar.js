36d612975436eac83f1f9942ce080b3f5bb71297
Eliiisa <elisaklein66@gmail.com>
2023-06-23 16:43:19 +0200

    autocomplétion barre de recherche (titre/ingrédients)

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,69 @@
-export default function SearchBar({ keyword, onChange }) {
-    return (
-        <div className="row">
-            <div className="col-8 m-auto">
-                <input
-                    type="text"
-                    className="form-control"
-                    placeholder={"Recherche par type ou par ingrédient"}
-                    key="search-bar"
-                    value={keyword}
-                    onChange={(e) => onChange(e.target.value)}
-                />
-            </div>
+import '../styles/search.css'
+
+export default function SearchBar({
+  keyword,
+  onChange,
+  onFocusIn,
+  onClickSuggestion,
+  showSuggestions,
+  optionsTitle,
+  optionsIngredient,
+}) {
+  const suggestionsTitle = optionsTitle.filter((option) =>
+    option.toLowerCase().includes(keyword.toLowerCase())
+  )
+  const suggestionsIngredient = optionsIngredient.filter((option) =>
+    option.toLowerCase().includes(keyword.toLowerCase())
+  )
+
+  return (
+    <div className="row">
+      <div className="col-8 m-auto">
+        <div className="search-bar">
+          <input
+            type="text"
+            className="form-control"
+            placeholder={'Recherche par type ou par ingrédient'}
+            key="search-bar"
+            value={keyword}
+            onChange={(e) => onChange(e.target.value)}
+            onFocus={onFocusIn}
+          />
+
+          {showSuggestions && (
+            <ul className="autocomplete">
+              {suggestionsTitle.length === 0 &&
+              suggestionsIngredient.length === 0 ? (
+                <span className="text-body-secondary">Aucun résultat</span>
+              ) : (
+                ''
+              )}
+              {suggestionsTitle.length > 0 && (
+                <span className="fw-bold">Titre recette</span>
+              )}
+              {suggestionsTitle.map((suggestion) => (
+                <li
+                  onClick={() => onClickSuggestion(suggestion)}
+                  key={suggestion}
+                >
+                  {suggestion}
+                </li>
+              ))}
+              {suggestionsIngredient.length > 0 && (
+                <span className="fw-bold">Ingrédients</span>
+              )}
+              {suggestionsIngredient.map((suggestion) => (
+                <li
+                  onClick={() => onClickSuggestion(suggestion)}
+                  key={suggestion}
+                >
+                  {suggestion}
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
-    )
+      </div>
+    </div>
+  )
 }
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,58 +1,106 @@
-import { useFetch } from "../utils/hooks/useFetch"
-import { useState } from "react"
-import SearchBar from "../components/SearchBar"
+import { useFetch } from '../utils/hooks/useFetch'
+import { useState, useEffect } from 'react'
+import SearchBar from '../components/SearchBar'
 import Card from '../components/common/Card'
 
 export default function Search() {
-    const { data, isLoading, error } = useFetch()
-    const recipes = data.recipes
-    const [keyword, setKeyword] = useState("")
-    const [results, setResults] = useState([])
+  const { data, isLoading, error } = useFetch()
+  const recipes = data.recipes
+  const [keyword, setKeyword] = useState('')
+  const [optionsTitle, setOptionsTitle] = useState([])
+  const [optionsIngredient, setOptionsIngredient] = useState([])
+  const [results, setResults] = useState([])
+  const [showSuggestions, setShowSuggestions] = useState(false)
 
-    const handleChange = (keyword) => {
-        let lowerCase = keyword.toLowerCase()
-        setKeyword(lowerCase)
+  useEffect(() => {
+    if (!isLoading) {
+      let listNames = [...new Set(recipes.map((recipe) => recipe.name))].sort()
 
-        filterData(lowerCase)
+      let listIngredients = [
+        ...new Set(
+          [].concat(
+            ...recipes.map((recipe) =>
+              recipe.ingredients.map((ingredient) => ingredient.name)
+            )
+          )
+        ),
+      ].sort()
+      setOptionsTitle(listNames)
+      setOptionsIngredient(listIngredients)
     }
+  }, [isLoading])
 
-    const filterData = (keyword) => {
-        if (keyword === "") setResults([])
-        else {
-            const filterData = recipes?.filter((recipe) => {
-                return (
-                    recipe.name.toLowerCase().includes(keyword) ||
-                    recipe.ingredients.some(ingredient => ingredient.name.toLowerCase().includes(keyword))
-                )
-            })
+  const handleFocusIn = () => {
+    setShowSuggestions(true)
+  }
 
-            console.log(filterData)
-            setResults(filterData)
-        }
+  const handleChange = (keyword) => {
+    setKeyword(keyword)
+    filterData(keyword)
+  }
+
+  const handleSuggestionClick = (suggestion) => {
+    setKeyword(suggestion)
+    setShowSuggestions(false)
+    filterData(suggestion.toLowerCase())
+  }
+
+  const filterData = (keyword) => {
+    if (keyword === '') setResults([])
+    else {
+      const filterData = recipes?.filter((recipe) => {
+        return (
+          recipe.name.toLowerCase().includes(keyword.toLowerCase()) ||
+          recipe.ingredients.some((ingredient) =>
+            ingredient.name.toLowerCase().includes(keyword.toLowerCase())
+          )
+        )
+      })
+      setResults(filterData)
     }
+  }
 
-
-    if (error) {
-        return <span>Erreur</span>
-    }
-    return (
-        <div>
-            <h1 className="text-center">Rechercher</h1>
-            <SearchBar keyword={keyword} onChange={handleChange} />
-            {isLoading ? (
-                <span>Loading</span>
-            ) : (
-                <div className="row">
-                    {results && results.map((recipe) => (
-                        <div className="col-4 p-4" key={recipe.id}>
-                            <Card recipe={recipe} />
-                        </div>
-                    ))}
-
+  if (error) {
+    return <span>Erreur</span>
+  }
+  return (
+    <div>
+      <h1 className="text-center">Rechercher</h1>
+      <SearchBar
+        keyword={keyword}
+        optionsTitle={optionsTitle}
+        optionsIngredient={optionsIngredient}
+        onFocusIn={handleFocusIn}
+        onChange={handleChange}
+        onClickSuggestion={handleSuggestionClick}
+        showSuggestions={showSuggestions}
+      />
+      {isLoading ? (
+        <span>Loading</span>
+      ) : (
+        <div className="row mt-3">
+          {results.length > 0 && (
+            <div className="col-12 text-end">
+              <span className="text-body-secondary">
+                {`${results.length} ${
+                  results.length > 1 ? 'résultats' : 'résultat'
+                }`}
+              </span>
+            </div>
+          )}
+          {results.length > 0
+            ? results.map((recipe) => (
+                <div className="col-4 p-4" key={recipe.id}>
+                  <Card recipe={recipe} />
                 </div>
-            )}
-
+              ))
+            : recipes.map((recipe) => (
+                <div className="col-4 p-4" key={recipe.id}>
+                  <Card recipe={recipe} />
+                </div>
+              ))}
         </div>
-
-    )
+      )}
+    </div>
+  )
 }
